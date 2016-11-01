cd33fe13a68dfd3323fdd89f638d1864d020e1f2
rmendez23 <rmendez2017@spyponders.com>
2016-11-01 17:15:41 -0400

    Update server_test1.py

--- server_test1.py
+++ server_test1.py
@@ -1,1 +1,44 @@
+#!/usr/bin/python
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+import atexit
+import socket
+#do I need to import pygame?
 
+host = ''        # Symbolic name meaning all available interfaces
+port = 12345     # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+s.listen(1)
+conn, addr = s.accept() #establishes connection?
+print('Connected by', addr)
+while True:
+	data = conn.recv(1024) #recieving data? data is newSpeed?
+	if not data: break
+	conn.sendall(data) # This sends the data recieved back to the client?
+conn.close() #move this?
+
+# create a default object, no changes to I2C address or frequency
+mh = Adafruit_MotorHAT(addr=0x60)
+
+# recommended for auto-disabling motors on shutdown!
+def turnOffMotors():
+	mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+atexit.register(turnOffMotors)
+
+myMotor = mh.getMotor(3)
+
+# set the speed to start, from 0 (off) to 255 (max speed)
+myMotor.setSpeed(0)
+
+if newSpeed<0:
+	myMotor.run(Adafruit_MotorHAT.BACKWARD)
+	print (newSpeed) #dont need?
+	myMotor.setSpeed(int(-newSpeed))
+	
+if newSpeed>0:
+	myMotor.run(Adafruit_MotorHAT.FORWARD)
+	print (newSpeed)myMotor.setSpeed(int(newSpeed))
