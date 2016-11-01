033b95c907cc9b2beefd30438b1545708f516440
rmendez23 <rmendez2017@spyponders.com>
2016-11-01 10:24:20 -0400

    Update test_client1.py

--- test_client1.py
+++ test_client1.py
@@ -24,3 +24,13 @@ try:
 		pygame.event.pump()
 		newSpeed = -250.0*j.get_axis(4)
 		print (newSpeed)
+
+#client socket		
+host = socket.gethostname()    
+port = 12345                   # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+s.sendall(b'Hello, world') #newSpeed in here?
+data = s.recv(1024)
+s.close()
+print('Received', repr(data))
