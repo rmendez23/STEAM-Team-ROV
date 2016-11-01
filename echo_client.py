3a5bb78e9b5f252f6d8593b26520136ea2cc04ac
rmendez23 <rmendez2017@spyponders.com>
2016-11-01 10:18:46 -0400

    Create echo_client.py

---
+++ echo_client.py
@@ -0,0 +1,11 @@
+# echo_client.py
+import socket
+
+host = socket.gethostname()    
+port = 12345                   # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+s.sendall(b'Hello, world')
+data = s.recv(1024)
+s.close()
+print('Received', repr(data))
