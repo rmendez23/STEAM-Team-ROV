fc10dec9d3de0ea8e5e593903ce17b268043c39b
rmendez23 <rmendez2017@spyponders.com>
2016-11-01 10:20:43 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
-
+#Joystick stuff
 import serial
 import pygame
 import time
+
+'''
+Gets joystick data and prints it
+'''
+pygame.init()
+#joystick.init()
+j = pygame.joystick.Joystick(0)
+j.init()
+print 'Initialized Joystick : %s' % j.get_name()
+
+# Keeps a history of buttons pressed so that one press does
+# not send multiple presses to the Arduino Board
+button_history = [0,0,0,0,0,0,0,0,0,0,0,0]
 
 try:
 	while (True):
