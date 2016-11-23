c53c2ad0f63121849747748fae75c44e28235568
rmendez23 <rmendez2017@spyponders.com>
2016-11-23 13:58:02 -0500

    Update echo_client.py

--- echo_client.py
+++ echo_client.py
@@ -1,7 +1,7 @@
 # echo_client.py
 import socket
 
-host = '10.66.66.6' #socket.gethostname()    
+host = '10.66.66.1' #socket.gethostname()    
 port = 12345                   # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
