5bae864732cd88dbfd3b73e160f8e9086442b861
rmendez23 <rmendez2017@spyponders.com>
2016-11-29 13:52:00 -0500

    add newline delimiter afyer objects

--- client_pi1.py
+++ client_pi1.py
@@ -23,6 +23,9 @@ print 'Initialized Joystick : %s' % j.get_name()
 # not send multiple presses to the Arduino Board
 button_history = [0,0,0,0,0,0,0,0,0,0,0,0]
 
+def reprNice(obj):
+	return repr(obj)+"\n"
+
 try:
 	#client socket		
 	host = '10.66.66.1' #socket.gethostname() change to IP of server   
@@ -38,13 +41,13 @@ try:
 				if e.type == pygame.JOYAXISMOTION:
 					newSpeed1 = -250.0*j.get_axis(4)
 					newSpeed2 = -250.0*j.get_axis(0)
-					s.sendall(repr({"command" : "FB", "speed" : newSpeed1}))
-					s.sendall(repr({"command" : "LR", "speed" : newSpeed2}))
+					s.sendall(reprNice({"command" : "FB", "speed" : newSpeed1}))
+					s.sendall(reprNice({"command" : "LR", "speed" : newSpeed2}))
 					#data = s.recv(1024)
 					#print('Received', repr(data))
 				if e.type == pygame.JOYHATMOTION:
 					newSpeed3 = -250.0*j.get_hat(hat_number)[1] #y position tuple of j.get_hat
-					s.sendall(repr({"command" : "UD", "speed" : newSpeed3}))
+					s.sendall(reprNice({"command" : "UD", "speed" : newSpeed3}))
 		except KeyboardInterrupt:
 			break
 		except:
