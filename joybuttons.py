2be8766151d1afe8ebbf0caf9f2d782fd05146a4
rmendez23 <rmendez2017@spyponders.com>
2016-11-30 16:01:59 -0500

    Update joybuttons.py

--- joybuttons.py
+++ joybuttons.py
@@ -4,18 +4,16 @@ import time
 pygame.init()
 j = pygame.joystick.Joystick(0)
 j.init()
-hat_number = j.get_numhats() - 1
+#hat_number = j.get_numhats() - 1
 print 'Initialized Joystick : %s' % j.get_name()
-print (hat_number)
+#print (hat_number)
 
 
 while True:
 	try:
 		pygame.event.wait()
 		myEvents = pygame.event.get()
-		for e in myEvents:
-			if e.type == pygame.JOYHATMOTION:
-				newSpeed3 = 250.0*j.get_hat(hat_number)[1] #y position tuple of j.get_hat
-				print(newSpeed3)
+		for x in range(0, j.get_numbuttons()-1):
+			print (j.get_button(x))
 	except KeyboardInterrupt:
 		j.quit()
