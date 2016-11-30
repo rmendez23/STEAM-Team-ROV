0ba447fd84f5da7aab060c7c1e5e6dbdb111d310
rmendez23 <rmendez2017@spyponders.com>
2016-11-30 15:55:03 -0500

    Update joyhat.py

--- joyhat.py
+++ joyhat.py
@@ -6,10 +6,13 @@ j = pygame.joystick.Joystick(0)
 j.init()
 hat_number = j.get_numhats() - 1
 print 'Initialized Joystick : %s' % j.get_name()
-pygame.event.wait()
-myEvents = pygame.event.get()
+print (hat_number)
+
+
 while True:
 	try:
+		pygame.event.wait()
+		myEvents = pygame.event.get()
 		for e in myEvents:
 			if e.type == pygame.JOYHATMOTION:
 				newSpeed3 = 250.0*j.get_hat(hat_number)[1] #y position tuple of j.get_hat
