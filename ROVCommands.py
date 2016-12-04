9beeb9c05df59529aa49de3bba178932b6f83f3c
rmendez23 <rmendez2017@spyponders.com>
2016-12-04 09:57:04 -0500

    Create ROVCommands.py

---
+++ ROVCommands.py
@@ -0,0 +1,43 @@
+#FORWARD and BACKWARD
+	if newSpeed1<0: #BOTH BACKWARD
+		myMotor3.run(Adafruit_MotorHAT.BACKWARD), myMotor2.run(Adafruit_MotorHAT.BACKWARD)
+		print ("Backward!", newSpeed1)
+		myMotor3.setSpeed(int(-newSpeed1)), myMotor2.setSpeed(int(-newSpeed1))
+		conn.sendall(reprNice({"message":["Backward!"], "speed" : [newSpeed1]}))
+
+	elif newSpeed1>0: #BOTH FORWARD
+		myMotor3.run(Adafruit_MotorHAT.FORWARD), myMotor2.run(Adafruit_MotorHAT.FORWARD)
+		print ("Forward!", newSpeed1)
+		myMotor3.setSpeed(int(newSpeed1)), myMotor2.setSpeed(int(newSpeed1))
+		conn.sendall(reprNice({"message":["Forward!"], "speed" : [newSpeed1]}))
+		
+	#RIGHT and LEFT
+	if newSpeed2<0: #LEFT
+		myMotor3.run(Adafruit_MotorHAT.FORWARD), myMotor2.run(Adafruit_MotorHAT.RELEASE)
+		print (newSpeed2)
+		myMotor3.setSpeed(int(-newSpeed2))
+		print("Turning left!", newSpeed2)
+		conn.sendall(reprNice({"message":["Turning Left!"], "speed" : [newSpeed2]}))
+		
+	elif newSpeed2>0: #RIGHT
+		myMotor2.run(Adafruit_MotorHAT.FORWARD), myMotor3.run(Adafruit_MotorHAT.RELEASE)
+		print (newSpeed2)
+		myMotor2.setSpeed(int(newSpeed2))
+		print("Turning right!", newSpeed2)
+		conn.sendall(reprNice({"message":["Turning Right!"], "speed" : [newSpeed2]}))
+
+	#UP and DOWN
+	if newSpeed3>0: #UP
+		myMotor1.run(Adafruit_MotorHAT.FORWARD)
+		print("Going Up!", newSpeed3)
+		conn.sendall(reprNice({"message":["Going Up!"], "speed" : [newSpeed3]}))
+
+	elif downSpeed>0: #DOWN
+		myMotor1.run(Adafruit_MotorHAT.BACKWARD)
+		print("Going Down!", downSpeed)
+		conn.sendall(reprNice({"message":["Going Down!"], "speed" : [downSpeed]}))
+
+	else: #RELEASE
+		myMotor3.run(Adafruit_MotorHAT.RELEASE), myMotor2.run(Adafruit_MotorHAT.RELEASE)
+		print("No action, waiting for command.")
+		conn.sendall(reprNice({"message":["No action, Waiting for command."], "speed" : [0.0]}))
