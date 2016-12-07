7e7d9eeb8b1c0769b129034c76af0078b14816fd
rmendez23 <rmendez2017@spyponders.com>
2016-12-07 16:25:03 -0500

    Update ROVCommands.py

--- ROVCommands.py
+++ ROVCommands.py
@@ -1,25 +1,25 @@
 #FORWARD and BACKWARD
-def ROVbackward():
+def ROVbackward(myMotor3, myMotor2, myMotor1):
 	myMotor3.run(Adafruit_MotorHAT.BACKWARD), myMotor2.run(Adafruit_MotorHAT.BACKWARD)
 	print ("Backward!", newSpeed1)
 	myMotor3.setSpeed(int(-newSpeed1)), myMotor2.setSpeed(int(-newSpeed1))
 	conn.sendall(reprNice({"message":["Backward!"], "speed" : [newSpeed1]}))
 
-def ROVforward():
+def ROVforward(myMotor3, myMotor2, myMotor1):
 	myMotor3.run(Adafruit_MotorHAT.FORWARD), myMotor2.run(Adafruit_MotorHAT.FORWARD)
 	print ("Forward!", newSpeed1)
 	myMotor3.setSpeed(int(newSpeed1)), myMotor2.setSpeed(int(newSpeed1))
 	conn.sendall(reprNice({"message":["Forward!"], "speed" : [newSpeed1]}))
 		
 	#RIGHT and LEFT
-def ROVleft():
+def ROVleft(myMotor3, myMotor2, myMotor1):
 	myMotor3.run(Adafruit_MotorHAT.FORWARD), myMotor2.run(Adafruit_MotorHAT.RELEASE)
 	print (newSpeed2)
 	myMotor3.setSpeed(int(-newSpeed2))
 	print("Turning left!", newSpeed2)
 	conn.sendall(reprNice({"message":["Turning Left!"], "speed" : [newSpeed2]}))
 		
-def ROVright():
+def ROVright(myMotor3, myMotor2, myMotor1):
 	myMotor2.run(Adafruit_MotorHAT.FORWARD), myMotor3.run(Adafruit_MotorHAT.RELEASE)
 	print (newSpeed2)
 	myMotor2.setSpeed(int(newSpeed2))
@@ -27,17 +27,17 @@ def ROVright():
 	conn.sendall(reprNice({"message":["Turning Right!"], "speed" : [newSpeed2]}))
 
 	#UP and DOWN
-def ROVup():
+def ROVup(myMotor3, myMotor2, myMotor1):
 	myMotor1.run(Adafruit_MotorHAT.FORWARD)
 	print("Going Up!", newSpeed3)
 	conn.sendall(reprNice({"message":["Going Up!"], "speed" : [newSpeed3]}))
 
-def ROVdown():
+def ROVdown(myMotor3, myMotor2, myMotor1):
 	myMotor1.run(Adafruit_MotorHAT.BACKWARD)
 	print("Going Down!", downSpeed)
 	conn.sendall(reprNice({"message":["Going Down!"], "speed" : [downSpeed]}))
 
-def ROVnoAction():
+def ROVnoAction(myMotor3, myMotor2, myMotor1):
 	#RELEASE ALL Motors
 		myMotor3.run(Adafruit_MotorHAT.RELEASE), myMotor2.run(Adafruit_MotorHAT.RELEASE)
 		print("No action, waiting for command.")
