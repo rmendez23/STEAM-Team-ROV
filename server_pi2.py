ea9cf7c037d31edea89bde82c9c34495a928607b
rmendez23 <rmendez2017@spyponders.com>
2016-12-21 15:20:16 -0500

    Update server_pi2.py

--- server_pi2.py
+++ server_pi2.py
@@ -84,3 +84,48 @@ while True:
 
 
 conn.close() #move this?
+
+#FORWARD and BACKWARD
+def ROVbackward(myMotor3, myMotor2, myMotor1):
+	myMotor3.run(Adafruit_MotorHAT.BACKWARD), myMotor2.run(Adafruit_MotorHAT.BACKWARD)
+	print ("Backward!", newSpeed1)
+	myMotor3.setSpeed(int(-newSpeed1)), myMotor2.setSpeed(int(-newSpeed1))
+	conn.sendall(reprNice({"message":["Backward!"], "speed" : [newSpeed1]}))
+
+def ROVforward(myMotor3, myMotor2, myMotor1):
+	myMotor3.run(Adafruit_MotorHAT.FORWARD), myMotor2.run(Adafruit_MotorHAT.FORWARD)
+	print ("Forward!", newSpeed1)
+	myMotor3.setSpeed(int(newSpeed1)), myMotor2.setSpeed(int(newSpeed1))
+	conn.sendall(reprNice({"message":["Forward!"], "speed" : [newSpeed1]}))
+		
+	#RIGHT and LEFT
+def ROVleft(myMotor3, myMotor2, myMotor1):
+	myMotor3.run(Adafruit_MotorHAT.FORWARD), myMotor2.run(Adafruit_MotorHAT.RELEASE)
+	print (newSpeed2)
+	myMotor3.setSpeed(int(-newSpeed2))
+	print("Turning left!", newSpeed2)
+	conn.sendall(reprNice({"message":["Turning Left!"], "speed" : [newSpeed2]}))
+		
+def ROVright(myMotor3, myMotor2, myMotor1):
+	myMotor2.run(Adafruit_MotorHAT.FORWARD), myMotor3.run(Adafruit_MotorHAT.RELEASE)
+	print (newSpeed2)
+	myMotor2.setSpeed(int(newSpeed2))
+	print("Turning right!", newSpeed2)
+	conn.sendall(reprNice({"message":["Turning Right!"], "speed" : [newSpeed2]}))
+
+	#UP and DOWN
+def ROVup(myMotor3, myMotor2, myMotor1):
+	myMotor1.run(Adafruit_MotorHAT.FORWARD)
+	print("Going Up!", newSpeed3)
+	conn.sendall(reprNice({"message":["Going Up!"], "speed" : [newSpeed3]}))
+
+def ROVdown(myMotor3, myMotor2, myMotor1):
+	myMotor1.run(Adafruit_MotorHAT.BACKWARD)
+	print("Going Down!", downSpeed)
+	conn.sendall(reprNice({"message":["Going Down!"], "speed" : [downSpeed]}))
+
+def ROVnoAction(myMotor3, myMotor2, myMotor1):
+	#RELEASE ALL Motors
+		myMotor3.run(Adafruit_MotorHAT.RELEASE), myMotor2.run(Adafruit_MotorHAT.RELEASE)
+		print("No action, waiting for command.")
+		conn.sendall(reprNice({"message":["No action, Waiting for command."], "speed" : [0.0]}))
