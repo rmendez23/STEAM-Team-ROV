e58a3a237e1a776931790766575bcf49dd387fd4
rmendez23 <rmendez2017@spyponders.com>
2016-12-28 19:26:46 -0500

    Update joybuttons.py

--- joybuttons.py
+++ joybuttons.py
@@ -4,7 +4,7 @@ import time
 import sys
 
 pygame.init()
-j = pygame.joystick.Joystick(0)
+pygame j = joystick.Joystick(0)
 j.init()
 #hat_number = j.get_numhats() - 1
 print ('Initialized Joystick : %s' % j.get_name())
